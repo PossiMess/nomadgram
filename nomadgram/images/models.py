8aeb8717558c7c4582986fd86e35ecf57537fa9d
Heonyoung Lee <>
2019-01-24 19:26:32 +0900

    find error

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -13,15 +13,18 @@ class Image(TimeStampedModel):
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return '{}-{}'.format(self.location, self.caption)
 
 class Comment(TimeStampedModel):
     """Comment Model"""
     message = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
 
 class Like(TimeStampedModel):
     """Like Model"""
-    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
